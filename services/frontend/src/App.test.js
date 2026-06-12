6b2b37e8ba777f15bfd126014540aa751bb07f34
Dinuka-SID <dmcoder01@gmail.com>
2026-06-12 16:30:58 +0530

    ci-cd updated

---
+++ services/frontend/src/App.test.js
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+test('dummy test to verify setup', () => {
+  expect(true).toBe(true);
+});
---
+++ services/user-service/test_app.py
@@ -0,0 +1,12 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_health(client):
+    rv = client.get('/health')
+    assert rv.status_code == 200
+    assert b"healthy" in rv.data
